b204d4ae2b8ac27e7a041bc460a2fa3abd3d00c7
Kynetiq <kinetikeith@protonmail.com>
2023-10-04 19:18:14 -0400

    Did basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Typography, Container, Stack } from '@mui/material';
+
 function App() {
-  return <></>;
+  return (
+    <Container>
+      <Stack direction="row" alignItems="center" justifyContent="center">
+        <Typography variant="h1" component="span">
+          Chromic
+        </Typography>
+      </Stack>
+    </Container>
+  );
 }
 
 export default App;
