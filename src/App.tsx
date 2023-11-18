28989d6f10b06f8d1c5ae67d4f3aef611c5d82fd
Kynetiq <kinetikeith@protonmail.com>
2023-11-18 11:05:51 -0500

    Added tabs/mode navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import chroma from 'chroma-js';
+import { Paper, Tabs, Tab } from '@mui/material';
 import SwatchMode from './modes/SwatchMode';
 import { useState } from 'react';
 import type { SwatchValue } from './components/Swatch';
@@ -11,9 +12,40 @@ const defaultValues = [
   chroma('#AB9F9D'),
 ].map((color) => ({ color, isLocked: false }));
 
+enum Mode {
+  Swatch,
+  Palette,
+  Export,
+}
+
 function App() {
   const [values, setValues] = useState<SwatchValue[]>(defaultValues);
-  return <SwatchMode direction="row" values={values} setValues={setValues} />;
+  const [mode, setMode] = useState<Mode>(Mode.Swatch);
+
+  return (
+    <>
+      <Paper
+        elevation={2}
+        sx={{
+          p: 1,
+          position: 'absolute',
+          left: '50%',
+          borderTopLeftRadius: 0,
+          borderTopRightRadius: 0,
+          transform: 'translateX(-50%)',
+        }}
+      >
+        <Tabs value={mode} onChange={(_event, newMode: Mode) => setMode(newMode)}>
+          <Tab label="Swatches" value={Mode.Swatch} />
+          <Tab label="Palette" value={Mode.Palette} />
+          <Tab label="Export" value={Mode.Export} />
+        </Tabs>
+      </Paper>
+      {mode === Mode.Swatch && (
+        <SwatchMode direction="row" values={values} setValues={setValues} />
+      )}
+    </>
+  );
 }
 
 export default App;
