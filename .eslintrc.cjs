f00a5f2bbb7b4b7eb2f05538f0f805d5b7256d16
kinetikeith <khman32@protonmail.com>
2024-02-16 17:02:28 -0500

    Migrated to preact

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:preact/recommended',
     'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
